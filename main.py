5a697553c4bc3157851cdf1e75c4e809ffd80f9a
Ronnzzy <abhineet.nim@gmail.com>
2025-04-20 10:30:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ if uploaded_file:
     name_column = st.selectbox("Select the column containing names", df.columns)
 
     # Keywords list (yours + extended, no removal)
-    keywords = [
-    non_individual_keywords = sorted(list(set[
+    non_individual_keywords = sorted(list(set([   
     "ACHYUT", "ACADEMIC", "ACCELERATOR", "ACCOUNTS", "ADMIN", "ADMINISTRATION", "ADULT", "ADVISORY",
     "ADVISORS", "AG", "AGENCY", "ALLIANCE", "APOTHECARY", "APS", "ASSOCIATES", "ASSOCIATION", "ASOCIACION",
     "AS", "A/S", "BANK", "BIO-TECHNE", "BIOTECH", "BOARD", "BORAD", "BTP", "BUREAU", "BV", "CABINET",
